57a40cc1f371665401515edc1b054d14662dbefd
ivanys23071996 <ivanys230796@gmail.com>
2022-01-12 13:54:18 +0200

    classwork_1

--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -6,16 +6,16 @@ import Launch from "../Launch/Launch";
 
 const Launches = () => {
     const [launches, setLaunches] = useState([]);
-
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
             .then(resolve => resolve.json())
             .then(value => setLaunches(value.filter(value=>value.launch_year!=='2020')))
+
     }, []);
 
     return (
         <div>
-            {launches.map(value => <Launch name={value.mission_name} year={value.launch_year} img={value.links.mission_patch_small}/>)}
+            {launches.map(value => <Launch key={value.id}  name={value.mission_name} year={value.launch_year} img={value.links.mission_patch_small}/>)}
         </div>
     );
 };
