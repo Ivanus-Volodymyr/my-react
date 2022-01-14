2ed6054d5fa89b8ce0e0f04dd4f13362cc31f6bc
ivanys23071996 <ivanys230796@gmail.com>
2022-01-14 11:07:18 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import Comments from "./components/Comments/Comments";
+
 
 
 function App() {
@@ -9,11 +8,6 @@ function App() {
         <div className="App">
             <div className={"UpBlock"}>
                 <Users/>
-                <Posts/>
-            </div>
-
-            <div className={"DownBlock"}>
-                <Comments/>
             </div>
         </div>
     );
