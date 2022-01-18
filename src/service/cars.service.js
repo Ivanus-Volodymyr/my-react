39b8febc80e2ced1040bd783b2ed9959d4112407
ivanys23071996 <ivanys230796@gmail.com>
2022-01-18 18:45:49 +0200

    homework_3

---
+++ src/service/cars.service.js
@@ -0,0 +1,10 @@
+import axiosService from "./axios.service";
+import {url} from "../config/urls";
+
+export const carsService = {
+    create: (car) => axiosService.post(url.cars, car).then(value => value.data),
+    getAll: () => axiosService.get(url.cars).then(value => value.data),
+    getById: (id) => axiosService.get(`${url.cars}/${id}`).then(value => value.data),
+    updateById: (id, car) => axiosService.patch(`${url.cars}/${id}`, car).then(value => value.data),
+    deleteById: (id) => axiosService.delete(`${url.cars}/${id}`)
+}
