4c16595455c7b539c7e7b1aa3fc15e21a65de11a
ivanys23071996 <ivanys230796@gmail.com>
2022-01-18 19:15:47 +0200

    homework_3

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import './User.css'
+import UserDetails from "../UserDetails/UserDetails";
 
-const User = ({user: {name, username, email}}) => {
+const User = ({user: {id, name, username, email}}) => {
 
+    const details = () => {
+        <UserDetails id={id}/>
+    }
     return (
         <div className={'User'}>
             <h3>{name}</h3>
             <div>{username}</div>
             <div>Email : {email}</div>
-            <button>User Details</button>
+            <button onClick={details}>User Details</button>
         </div>
     );
 };
--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
 const UserDetails = () => {
+
     return (
         <div>
-            <h3>{name}</h3>
-            <div>{username}</div>
-            <div>Email : {email}</div>
-            <ul><h4>Address</h4>
-                <li>Street : {street}</li>
-                <li>Suite : {suite}</li>
-                <li>City : {city}</li>
-                <li>Zipcode : {zipcode}</li>
-                <li>Lat : {lat}</li>
-                <li>Lng : {lng}</li>
-            </ul>
-            <div>Phone : {phone}</div>
-            <div>Website : {website}</div>
-            <ul><h4>Company</h4>
-                <li>CompanyName : {companyName}</li>
-                <li>CompanyCatchPhrase : {companyCatchPhrase}</li>
-                <li>CompanyBs : {companyBs}</li>
-            </ul>
+            {/*<h3>{name}</h3>*/}
+            {/*<div>{username}</div>*/}
+            {/*<div>Email : {email}</div>*/}
+            {/*<ul><h4>Address</h4>*/}
+            {/*    <li>Street : {street}</li>*/}
+            {/*    <li>Suite : {suite}</li>*/}
+            {/*    <li>City : {city}</li>*/}
+            {/*    <li>Zipcode : {zipcode}</li>*/}
+            {/*    <li>Lat : {lat}</li>*/}
+            {/*    <li>Lng : {lng}</li>*/}
+            {/*</ul>*/}
+            {/*<div>Phone : {phone}</div>*/}
+            {/*<div>Website : {website}</div>*/}
+            {/*<ul><h4>Company</h4>*/}
+            {/*    <li>CompanyName : {companyName}</li>*/}
+            {/*    <li>CompanyCatchPhrase : {companyCatchPhrase}</li>*/}
+            {/*    <li>CompanyBs : {companyBs}</li>*/}
+            {/*</ul>*/}
         </div>
     );
 };
