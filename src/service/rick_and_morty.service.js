74c1c313fd9cfaae93197bd8cf93d3467a26f935
ivanys23071996 <ivanys230796@gmail.com>
2022-01-25 22:52:53 +0200

    rick_and_morty

--- src/service/rick_and_morty.service.js
+++ src/service/rick_and_morty.service.js
@@ -3,5 +3,6 @@ import {url} from "../config/urls";
 
 
 export const rick_and_mortyService = {
-    getAll: () => axiosService.get(url.episode).then(value => value.data)
+    getAll: () => axiosService.get(url.episode).then(value => value.data),
+    getById: (id) => axiosService.get(url.episode`/${id}`).then(value => value.data)
 }
