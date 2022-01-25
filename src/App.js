b5cd17d3d1289b63fbcb7910cbb5cba2cea0ee39
ivanys23071996 <ivanys230796@gmail.com>
2022-01-25 22:29:09 +0200

    rick_and_morty

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import './App.css';
-import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import Comments from "./components/Comments/Comments";
-
 
 function App() {
     return (
         <div className="App">
-            <div className={"UpBlock"}>
-                <Users/>
-                <Posts/>
-            </div>
 
-            <div className={"DownBlock"}>
-                <Comments/>
-            </div>
         </div>
     );
 }
